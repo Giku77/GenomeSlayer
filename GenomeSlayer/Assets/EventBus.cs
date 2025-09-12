58abd1afb9266eefe1769e8403e5e1ef97c93abd
길하영 <a2207624435@gmail.com>
2025-09-12 17:35:07 +0900

    적 아이템 드롭 구현 및 비료, 씨앗 아이템 추가

--- GenomeSlayer/Assets/EventBus.cs
+++ GenomeSlayer/Assets/EventBus.cs
@@ -4,6 +4,7 @@ using System;
 public static class EventBus
 {
     public static Action EnemyDied;
+    public static Action<Vector3> EnemyDropSeed;
     public static Action<int> WaveStarted;
     //public static Action<TreeEntity> TreeGrown;
     public static Action<int> PointsChanged;
