497eb439402a94090df8aed87c35db086e420c8a
길하영 <a2207624435@gmail.com>
2025-09-12 17:58:18 +0900

    아이템 데이터 정의

--- GenomeSlayer/Assets/Project/Scripts/Item.cs
+++ GenomeSlayer/Assets/Project/Scripts/Item.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public class ItemData
+    {
+        public string itemName;
+        public int itemID;
+        public string description;
+        public Sprite icon;
+    }
+    public ItemData itemData;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
